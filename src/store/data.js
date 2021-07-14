a1477bb0a079333e2b8955de566d9fc1a09e56b3
李毅桐 <80571620+lyt-wcl@users.noreply.github.com>
2021-07-14 16:12:57 +0800

    1

--- src/store/data.js
+++ src/store/data.js
@@ -1,9 +1,10 @@
-
+//import usaJson from 'public\USA.json'
 // 初始数据
 const initialState = {
     count: 0,
     data1: [],
     data2: [],
+//    usaJSon: usaJson,
 };
 
 export default initialState;
